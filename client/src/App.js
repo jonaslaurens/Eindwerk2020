8eca167785fd15c020716f21959f8331ac7aa2ee
Jonas Laurens <lauwe505@gmail.com>
2020-06-29 16:14:45 +0200

    added axios and material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import LoginPage from './components/Login';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello there</h1>
+      <LoginPage />
     </div>
   );
 }
---
+++ client/src/components/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <form action="">
+        <p>here</p>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
