df8e6a70661a0fd7e067390c0f516dd1efbbface
Jonas Laurens <lauwe505@gmail.com>
2020-06-29 19:22:42 +0200

    added login page

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import {
+  Grid,
+  Container,
+  FormGroup,
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+} from '@material-ui/core';
+
+import style from './LoginPage.module.css';
 
 const LoginPage = () => {
   return (
-    <div>
-      <form action="">
-        <p>here</p>
-      </form>
-    </div>
+    <Grid
+      container
+      justify="center"
+      alignContent="center"
+      direction="column"
+      className={style.login}
+    >
+      <h3>Login to start playing</h3>
+      <FormGroup>
+        <FormControl>
+          <InputLabel htmlFor="my-input">Name</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">
+            We'll never share your email.
+          </FormHelperText>
+        </FormControl>
+      </FormGroup>
+    </Grid>
   );
 };
 
