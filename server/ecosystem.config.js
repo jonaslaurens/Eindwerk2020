e96d25cec2ebd8b409512b35bb63607cc736749a
Jonas Laurens <lauwe505@gmail.com>
2020-07-28 14:41:44 +0200

    changed some scripts

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-  apps : [{
-    name: 'casino',
-    script: 'index.js',
-    instances: 1,
-    autorestart: true,
-    ignore_watch : ["node_modules"],
-    watch_options: {
-      "followSymlinks": false
+  apps: [
+    {
+      name: 'casino',
+      script: './index.js',
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      ignore_watch: ['node_modules'],
+      watch_options: {
+        followSymlinks: false,
+      },
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
+  ],
 };
