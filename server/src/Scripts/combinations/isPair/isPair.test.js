82ed5a4890d6a34f7cb2e8c4a303e872e726a54e
Jonas <lauwe505@gmail.com>
2020-08-04 15:14:29 +0200

    added three of a kind and quads test

--- server/src/Scripts/combinations/isPair/isPair.test.js
+++ server/src/Scripts/combinations/isPair/isPair.test.js
@@ -29,4 +29,32 @@ describe('test if given array contains the combination pair', () => {
 
     expect(isPair(countCards(cards), 2)).toEqual(false);
   });
+
+  test('We expect true cause we got 3 of a kind', () => {
+    const cards = [
+      { value: 2, suit: 'Clubs' },
+      { value: 4, suit: 'Clubs' },
+      { value: 3, suit: 'Clubs' },
+      { value: 2, suit: 'Hearts' },
+      { value: 13, suit: 'Hearts' },
+      { value: 4, suit: 'Clubs' },
+      { value: 2, suit: 'Diamonds' },
+    ];
+
+    expect(isPair(countCards(cards), 3)).toEqual(true);
+  });
+
+  test('We expect true cause we got quads', () => {
+    const cards = [
+      { value: 2, suit: 'Clubs' },
+      { value: 4, suit: 'Clubs' },
+      { value: 3, suit: 'Clubs' },
+      { value: 2, suit: 'Hearts' },
+      { value: 2, suit: 'Spades' },
+      { value: 4, suit: 'Clubs' },
+      { value: 2, suit: 'Diamonds' },
+    ];
+
+    expect(isPair(countCards(cards), 4)).toEqual(true);
+  });
 });
