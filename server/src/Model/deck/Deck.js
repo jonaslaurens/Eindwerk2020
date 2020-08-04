2aa7837342a66512e8902024171f88948e113704
Jonas <lauwe505@gmail.com>
2020-08-04 20:23:15 +0200

    removed unused methods in Deck class

--- server/src/Model/deck/Deck.js
+++ server/src/Model/deck/Deck.js
@@ -16,31 +16,12 @@ class Deck {
    * @description creates a new deck of cards
    */
   createDeck() {
-    SUITS.forEach(suit => {
-      Object.entries(VALUES).forEach(value => {
+    SUITS.forEach((suit) => {
+      Object.entries(VALUES).forEach((value) => {
         const newCard = new Card(value[1], suit);
         this.deck.push(newCard);
       });
     });
-  }
-
-  /**
-   *  @description shuffles the deck in random order.
-   */
-  shuffleDeck() {
-    for (let i = this.deck.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
-    }
-  }
-
-  /**
-   * @description removes a card from the deck and gives it to the applicant
-   * @returns { object } the card that has been dealt
-   */
-  dealCard() {
-    const dealtCard = this.deck.pop();
-    return dealtCard;
   }
 }
 
