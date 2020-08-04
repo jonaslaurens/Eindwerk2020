a42465c98d666056011e4e1d0bd37a956659fb39
Jonas <lauwe505@gmail.com>
2020-08-04 15:47:04 +0200

    royal flush fixed

--- server/src/Scripts/combinations/isRoyalFlush/index.js
+++ server/src/Scripts/combinations/isRoyalFlush/index.js
@@ -3,9 +3,17 @@ const isHighCard = require('../isHighCard');
 const isLowStraight = require('../isLowStraight');
 
 module.exports = isRoyalFlush = (cards, countedCards, countedSuits) => {
+  // check if we got a straightflush and if there is an ace
   if (isStraightFlush(cards, countedSuits) && isHighCard(countedCards) === 14) {
-    // if the straight is the lowest we can have we return false
-    if (!isLowStraight(countedCards)) return true;
+    // if we get here it means we have hit a straightflush after this we check if it's the highest straight we can have
+    if (
+      countedCards[13] > 0 &&
+      countedCards[12] > 0 &&
+      countedCards[11] > 0 &&
+      countedCards[10] > 0 &&
+      countedCards[9] > 0
+    )
+      return true;
   }
 
   return false;
--- server/src/Scripts/combinations/isRoyalFlush/isRoyalFlush.test.js
+++ server/src/Scripts/combinations/isRoyalFlush/isRoyalFlush.test.js
@@ -19,6 +19,22 @@ describe('test if given array contains the combination royal flush', () => {
     );
   });
 
+  test('Result should be a false, we only hit a straight flush', () => {
+    const cards = [
+      { value: 1, suit: 'Clubs' },
+      { value: 3, suit: 'Diamonds' },
+      { value: 8, suit: 'Clubs' },
+      { value: 10, suit: 'Clubs' },
+      { value: 11, suit: 'Clubs' },
+      { value: 12, suit: 'Clubs' },
+      { value: 9, suit: 'Clubs' },
+    ];
+
+    expect(isRoyalFlush(cards, countCards(cards), countSuits(cards))).toEqual(
+      false
+    );
+  });
+
   test('Result should be false', () => {
     const cards = [
       { value: 1, suit: 'Clubs' },
